1c06365318d88238b102b659e8d7c9d2a435bedc
Gelas10 <tasosgelas@hotmail.com>
2015-12-25 17:28:55 +0200

    Added 2 Classes and 3 sample files

---
+++ Papadopo/src/main/IndexWorker.java
@@ -0,0 +1,14 @@
+package main;
+
+public class IndexWorker extends Thread 
+{
+	public IndexWorker()
+	{
+		
+	}
+	
+	public void run()
+	{
+		System.out.println("I runned!");
+	}
+}
---
+++ Papadopo/src/main/InvertedIndex.java
@@ -0,0 +1,9 @@
+package main;
+
+public class InvertedIndex 
+{
+	public InvertedIndex()
+	{
+		
+	}
+}
