e88df9474d5d17399cb0472cb098fb57a3148f59
Gelas10 <tasosgelas@hotmail.com>
2015-12-25 17:24:35 +0200

    Reading files

--- Papadopo/src/main/Papadopo.java
+++ Papadopo/src/main/Papadopo.java
@@ -1,16 +1,31 @@
 package main;
 
-import java.util.ArrayList;
+import java.io.*;
+import java.util.*;
 
 public class Papadopo 
 {
 
 	public static void main(String[] args) 
 	{
-		// TODO Auto-generated method stub
-		System.out.println("Testing");
-		System.out.println("seon");
-		ArrayList<String> list=new ArrayList<>();
+		// Reading files named [id].txt ( example : 1.txt )
+		boolean finished=false;
+		int count=1;
+		do
+		{
+			String filename=count+".txt";
+			try (BufferedReader reader=new BufferedReader(new FileReader(filename)))
+			{
+				System.out.println(count+")"+reader.readLine());
+				count++;
+			} catch (IOException e) 
+			{
+				finished=true;
+			}
+			
+		}while(!finished);
+		
+		
 	}
 
 }
