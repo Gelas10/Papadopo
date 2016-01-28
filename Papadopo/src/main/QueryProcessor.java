0810691b06e718a48b83a5cca60aad251a9b26b7
thanelmas <thanasiselmas@gmail.com>
2016-01-28 12:32:16 +0200

    added JavaDoc to QueryProcessor.

--- Papadopo/src/main/QueryProcessor.java
+++ Papadopo/src/main/QueryProcessor.java
@@ -8,13 +8,18 @@ import java.util.Map.Entry;
 import java.util.StringTokenizer;
 
 
-
+/**
+ * 
+ * */
 public class QueryProcessor {
 	
 	//TODO Make these variables private
 	
+	
 	public InvertedIndex index;
 	public double norms[];
+	
+	/**An array that contains the "vector" of word weights of every document*/
 	public ArrayList<HashMap<String,Double>> vectors;
 	public double similarity[];
 	private double Ld[];
@@ -22,8 +27,11 @@ public class QueryProcessor {
 	public int queryID = -1;
 	public double queryNorm;
 	public HashMap<String,Double> queryVector;
+	
+	/**An inverted index built on the words of the query only*/
 	public InvertedIndex queryInvIndex;
 	
+	/**A simple representation of a "vector", that is the actual weights it contains and it's norm.*/
 	public class Vector{
 		private HashMap<String,Double> vector;
 		private double norm;
@@ -63,6 +71,16 @@ public class QueryProcessor {
 		}		
 	}
 	
+	/**
+	 * Creates the the "vector" that contains the weight of each word of the document.
+	 * The weight of a word inside this "vector" is: tf*idf (of this word).
+	 * Note: The "vector" is actually a HashMap that maps Word to Weight.
+	 * Note: tf and idf of a word don't change, so if we see a word once inside a document, the weight for this word has been computed
+	 * and we do not need to recompute it every time we see this word again inside the document.
+	 * @param document : the full text of the document (all words)
+	 * @param docID : the id of this document, that is (>=0 for all documents) or (=-1 for the query)
+	 * @param index : an inverted index built for all documents
+	 * */
 	public Vector computeVector(String document, int docID, InvertedIndex index){
 		
 		double norm = 0;
@@ -74,18 +92,19 @@ public class QueryProcessor {
 			
 			String word = words.nextToken();
 		
-			HashMap<Integer,MutableInt> docsMap = index.getHashMap().get(word);
-			
-			int freqInThisDocument = docsMap.get(docID).get();
-			int nt;//how many documents contain this word.
-			
-			nt = docsMap.size();
-
-			double idf = Math.log( 1 + documents/(double)nt);
-			double tf = 1 + Math.log(freqInThisDocument);
-			
 			//Insert a weight only the first time you see this word.
 			if(!vector.containsKey(word)){
+			
+				HashMap<Integer,MutableInt> docsMap = index.getHashMap().get(word);
+				
+				int freqInThisDocument = docsMap.get(docID).get();
+				int nt;//how many documents contain this word.
+				
+				nt = docsMap.size();
+	
+				double idf = Math.log( 1 + documents/(double)nt);
+				double tf = 1 + Math.log(freqInThisDocument);
+			
 				System.out.println("weightInDoc"+docID+"("+word+") = "+tf+"*"+idf+" = "+(tf*idf));
 				vector.put(word, tf*idf );
 				norm += Math.pow(tf*idf, 2);
@@ -100,14 +119,23 @@ public class QueryProcessor {
 		
 	}
 	
+	/**
+	 * Computes similarity between a document and a query.
+	 * The similarity of this document with the query is in fact an accumulator (just a number to which we only add) initialized to zero.
+	 * For every word of the query that appears inside the document, we add this: (weightOfWordInDoc*weightOfWordInQuery) to the accumulator.
+	 * ATTENTION: This function scans the query and computes the similarity for one document.
+	 * We need to modify it to: scans the query and gradually computes all similarities.
+	 * @param qvector : The "vector" of the query (maps Word to Weight)
+	 * @param qnorm : The norm of the query vector
+	 * */
 	public double computeCosineSimilarity(HashMap<String,Double> qvector , double qnorm, HashMap<String,Double> dvector , double dnorm){
 		
 		double similarity = 0;
 		
 		//For each query term
-		for (Entry<String, Double> wordEntry : qvector.entrySet()){
+		for (Entry<String, Double> queryTerm : qvector.entrySet()){
 			
-			String word = wordEntry.getKey();
+			String word = queryTerm.getKey();
 			
 			//Document contains query term
 			if(dvector.containsKey(word)){
@@ -128,6 +156,10 @@ public class QueryProcessor {
 		
 	}
 	
+	/**
+	 * Creates an inverted index containing the words of the query only and computes the "vector" and norm of the query.
+	 * @param queryString : the full text of the query (all it's words)
+	 * */
 	public void setQuery(String queryString){	
 		
 		//Create an inverted index for the query ONLY
@@ -182,47 +214,47 @@ public class QueryProcessor {
 //			testing.clear();											//--//
 //		}
 //	}
-	
-	public void computeSimilarityUsingTopkAlgorithm(){
-		
-		HashMap<Integer,Double> scores = new HashMap<Integer,Double>();
-		HashMap<String,HashMap<Integer, MutableInt>> index = this.index.getHashMap();
-		
-		String query[] = {"ant","bee","goat","chair","melon"};
-		
-		//For each query term
-		for(int i=0;i<query.length;i++){
-			
-			HashMap<Integer,MutableInt> docsMap = index.get(query[i]);
-			double idf = Math.log( 1 + documents/(double)docsMap.size());
-			
-			//For each document with this term
-			for (Entry<Integer, MutableInt> doc : docsMap.entrySet()){
-				
-				double tf = 1 + Math.log(doc.getValue().get());
-				
-				//Does this doc already have some score?
-				if( scores.containsKey(doc.getKey()) ){
-					
-					scores.put(doc.getKey() , tf*idf + scores.get(doc.getKey()));
-				}else{
-					
-					scores.put(doc.getKey() , tf*idf);
-				}
-			}
-		}
-		
-		//Normalize each document's score
-		for (Entry<Integer, Double> score : scores.entrySet()){
-			int docID = score.getKey();
-			scores.put(docID, score.getValue()/Ld[docID]);
-		}
-		
-		//Print the similarity results:
-		for (Entry<Integer, Double> score : scores.entrySet()){
-			System.out.println("Similarity(query,doc"+score.getKey()+": "+score.getValue());
-		}
-	}
+//	
+//	public void computeSimilarityUsingTopkAlgorithm(){
+//		
+//		HashMap<Integer,Double> scores = new HashMap<Integer,Double>();
+//		HashMap<String,HashMap<Integer, MutableInt>> index = this.index.getHashMap();
+//		
+//		String query[] = {"ant","bee","goat","chair","melon"};
+//		
+//		//For each query term
+//		for(int i=0;i<query.length;i++){
+//			
+//			HashMap<Integer,MutableInt> docsMap = index.get(query[i]);
+//			double idf = Math.log( 1 + documents/(double)docsMap.size());
+//			
+//			//For each document with this term
+//			for (Entry<Integer, MutableInt> doc : docsMap.entrySet()){
+//				
+//				double tf = 1 + Math.log(doc.getValue().get());
+//				
+//				//Does this doc already have some score?
+//				if( scores.containsKey(doc.getKey()) ){
+//					
+//					scores.put(doc.getKey() , tf*idf + scores.get(doc.getKey()));
+//				}else{
+//					
+//					scores.put(doc.getKey() , tf*idf);
+//				}
+//			}
+//		}
+//		
+//		//Normalize each document's score
+//		for (Entry<Integer, Double> score : scores.entrySet()){
+//			int docID = score.getKey();
+//			scores.put(docID, score.getValue()/Ld[docID]);
+//		}
+//		
+//		//Print the similarity results:
+//		for (Entry<Integer, Double> score : scores.entrySet()){
+//			System.out.println("Similarity(query,doc"+score.getKey()+": "+score.getValue());
+//		}
+//	}
 		
 	public static void main(String[] args){
 	
