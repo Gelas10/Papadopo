4a8d70e64674a191433cbb7832034342cbb5b154
thanelmas <thanasiselmas@gmail.com>
2016-01-29 19:00:56 +0200

    QueryProcessor: change logic from sequential to parallel (for now everything runs sequentially though)

--- Papadopo/src/main/QueryProcessor.java
+++ Papadopo/src/main/QueryProcessor.java
@@ -1,6 +1,11 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
 import java.util.StringTokenizer;
 
@@ -21,24 +26,10 @@ public class QueryProcessor {
 	public int documents;
 	public int queryID = -1;
 	public double queryNorm;
-	public HashMap<String,Double> queryVector;
+	public Map<String,Double> queryVector;
 	
 	/**An inverted index built on the words of the query only*/
-	public InvertedIndex queryInvIndex;
-	
-	/**A simple representation of a "vector", that is the actual weights it contains and it's norm.*/
-	public class Vector{
-		private HashMap<String,Double> vector;
-		private double norm;
-		
-		public Vector(HashMap<String,Double> vector1, double norm1){
-			vector = vector1;
-			norm = norm1;
-		}
-		
-		public HashMap<String,Double> getVector(){return vector;}
-		public double getNorm(){return norm;}
-	}
+	public HashMap<String,Integer> queryFrequencies;
 	
 	public QueryProcessor(String docs[])
 	{
@@ -62,140 +53,274 @@ public class QueryProcessor {
 	}
 	
 	/**
-	 * Creates the the "vector" that contains the weight of each word of the document.
+	 * Fills a part of a document's "vector".
+	 * Given a document's "chunk" of some words, this function puts the weights of the words of the "chunk" in the "vector".
 	 * The weight of a word inside this "vector" is: tf*idf (of this word).
-	 * Note: The "vector" is actually a HashMap that maps Word to Weight.
-	 * Note: tf and idf of a word don't change, so if we see a word once inside a document, the weight for this word has been computed
-	 * and we do not need to recompute it every time we see this word again inside the document.
-	 * @param document : the full text of the document (all words)
+	 * @param vector : a shared Map where this function is going to write/read (other threads may also be using this shared Map).
+	 * @param norm : a shared Double that is incremented (other threads may also be using this shared Double).
+	 * @param document : a "chunk" of this document (some words of the document) 
 	 * @param docID : the id of this document, that is (>=0 for all documents) or (=-1 for the query)
 	 * @param index : an inverted index built for all documents
 	 * */
-	public Vector computeVector(String document, int docID, InvertedIndex index){
-		
-		double norm = 0;
-		HashMap<String,Double> vector = new HashMap<String,Double>();
+	public void computeVector(Map<String,Double> vector, MutableDouble norm, List<String> document, int docID, InvertedIndex index){
 
-		//Read the document and for each word insert a weight to "vector" 
-		StringTokenizer words = new StringTokenizer(document);
-		while(words.hasMoreTokens()){
-			
-			String word = words.nextToken();
+		 //* NOTE1: The "vector" is actually a HashMap that maps Word to Weight.
+		 //* NOTE2: tf and idf of a word don't change, so if we see that the weight of a word is already set, there is no need to recompute and reset it.
+		 //* NOTE3: N is the number of documents (the query does not count)
+		 //* NOTE4: nt is the number of documents that contain a given word. It is assumed that the query participates in this number (example: 2 documents contain "hello", query contains "hello", so nt=2+1) 
 		
-			//Insert a weight only the first time you see this word.
+		//Read the document and for each word insert a weight to "vector" 
+		Iterator<String> words = document.iterator();
+		while(words.hasNext()){
+			
+			String word = words.next();
+		
+					//Insert a weight only the first time you see this word (Is the weight already set? If yes, you are done.).
+					//Concurrency Note: 
+					//Scenario: Thread1 and Thread2 ask "does vector contain the word 'apple'?" and both get no as an answer.
+					//In this case, they will both (one at a time) write the same value 0.32 (for example) inside the map.
+					//So Thread1 puts 0.32 as the mapped value of 'apple' and then Thread2 replaces 0.32 with 0.32.
+					//This will only happen when the word 'apple' has no mappings. Once a mapping is created, this effect disappears.
+			
 			if(!vector.containsKey(word)){
 			
 				HashMap<Integer,MutableInt> docsMap = index.getHashMap().get(word);
 				
 				int freqInThisDocument = docsMap.get(docID).get();
-				int nt;//how many documents contain this word.
 				
+				//How many documents (OR query) contain this word?
+				int nt;
 				nt = docsMap.size();
+				//if(queryFrequencies.containsKey(word)){nt++;}
 	
+				//Compute tf, idf
 				double idf = Math.log( 1 + documents/(double)nt);
 				double tf = 1 + Math.log(freqInThisDocument);
 			
-				System.out.println("weightInDoc"+docID+"("+word+") = "+tf+"*"+idf+" = "+(tf*idf));
+				//Write to the shared structures
+				System.out.println("weightInDoc"+docID+"("+word+") = "+" [1+log("+freqInThisDocument+")]*[log(1+"+documents+"/"+nt+")] = "+tf+"*"+idf+" = "+(tf*idf));
 				vector.put(word, tf*idf );
-				norm += Math.pow(tf*idf, 2);
+				norm.incrementBy(Math.pow(tf*idf, 2));
+				System.out.println("norm += (weight)^2 = ("+(tf*idf)+")^2 = "+Math.pow(tf*idf, 2));
 			}
+		}		
+	}
+	
+	
+	/**
+	 * Fills a part of the query's "vector".
+	 * Given a query's "chunk" of some words, this function puts the weights of the words of the "chunk" in the "vector".
+	 * The weight of a word inside this "vector" is: tf*idf (of this word).
+	 * @param vector : a shared Map where this function is going to write/read (other threads may also be using this shared Map).
+	 * @param norm : a shared Double that is incremented (other threads may also be using this shared Double).
+	 * @param query : a "chunk" of the query (some words of the document) 
+	 * @param index : an inverted index built for all documents (except the query). The query word frequencies are stored in a QueryProcessor Map.
+	 * */
+	public void computeQueryVector(Map<String,Double> vector, MutableDouble norm, List<String> query, InvertedIndex index){
+
+		//Read the document and for each word insert a weight to "vector" 
+		Iterator<String> words = query.iterator();
+		while(words.hasNext()){
 			
-		}
-		
-		norm = Math.sqrt(norm);
-		System.out.println("norm("+docID+"): "+norm+"\n-------------------------------------------------------------------------");
-		
-		return new Vector(vector,norm);
-		
+			String word = words.next();
+			
+			if(!vector.containsKey(word)){
+			
+				HashMap<Integer,MutableInt> docsMap = index.getHashMap().get(word);
+				
+				int freqInThisDocument = queryFrequencies.get(word);
+				
+				//How many documents (AND query) contain this word?
+				int nt;
+				nt = docsMap.size();
+				//nt++;//For the query
+	
+				//Compute tf,idf
+				double idf = Math.log( 1 + documents/(double)nt);
+				double tf = 1 + Math.log(freqInThisDocument);
+			
+				//Write to the shared structures
+				System.out.println("weightInDoc"+queryID+"("+word+") = "+" [1+log("+freqInThisDocument+")]*[log(1+"+documents+"/"+nt+")] = "+tf+"*"+idf+" = "+(tf*idf));
+				vector.put(word, tf*idf );
+				norm.incrementBy(Math.pow(tf*idf, 2));
+				System.out.println("norm += (weight)^2 = ("+(tf*idf)+")^2 = "+Math.pow(tf*idf, 2));
+			}
+		}		
 	}
 	
 	/**
-	 * Creates an inverted index containing the words of the query only and computes the "vector" and norm of the query.
-	 * @param queryString : the full text of the query (all it's words)
+	 * Given a query "q", for each query word "w", for each document "d" that contains that word, add {weight(w,d)*weight(w,q)} to the similarity of "d" with the query.
 	 * */
-	public void setQuery(String queryString){	
+	public void computeSimilarities(){
 		
-		//Create an inverted index for the query ONLY
-		queryInvIndex = new InvertedIndex();
+		//For each query word
+		for (Entry<String, Double> queryTerm : queryVector.entrySet()){
+					
+			String word = queryTerm.getKey();
+			System.out.println(word+" (of query)");
 			
-		StringTokenizer words = new StringTokenizer(queryString);
-		while(words.hasMoreTokens()){
-			String word = words.nextToken();
-			queryInvIndex.put(word, queryID);
+			//For each document that contains this word
+			for (Entry<Integer, MutableInt> doc : index.getHashMap().get(word).entrySet()){
+				
+				int docID = doc.getKey();
+				
+				//Read the weight of this word in the query and in the document
+				double weightOfWordInQuery = queryVector.get(word);
+				double weightOfWordInDocument = vectors.get(docID).getVector().get(word);
+				
+				//Add something to the similarity of this document (with the query)
+				if(similarity.containsKey(docID)){
+					similarity.put(docID, similarity.get(docID) + (weightOfWordInQuery*weightOfWordInDocument));
+				}else{
+					similarity.put(docID,weightOfWordInQuery*weightOfWordInDocument);
+				}
+				
+				System.out.println("\tsim(doc"+docID+") += "+"weightQuery["+word+"]*weightDoc["+word+"] = "+weightOfWordInQuery+"*"+weightOfWordInDocument+" = "+(weightOfWordInQuery*weightOfWordInDocument));
+			}
 		}
 		
-		//Compute the query vector and norm
-		Vector v1 = computeVector(queryString, queryID , queryInvIndex);
-		queryNorm = v1.getNorm();
-		queryVector = v1.getVector();
+		System.out.println("\n\n");
 		
+		//Replace similarities with normalized similarities
+		for (Entry<Integer, Double> similarities : similarity.entrySet()){
+			
+			int docID = similarities.getKey();
+			
+			double notNormalizedSimilarity = similarities.getValue();
+			double docNorm   = vectors.get(docID).getNorm();
+			
+			System.out.println("Normalize sim(doc"+docID+","+"query): "+notNormalizedSimilarity+" -> "+(notNormalizedSimilarity/(docNorm*queryNorm)));
+			similarities.setValue(notNormalizedSimilarity/(docNorm*queryNorm));
+		}
+	}
+	
+	
+	/**
+	 * Counts the word frequency for each word in the query and computes the "vector" and norm of the query.
+	 * @param queryString : the full text of the query (all it's words)
+	 * */
+	public void setQuery(List<String> query){	
+		
+		//Count the frequency of each word inside the query
+		queryFrequencies = new HashMap<String,Integer>();
+
+		//Count the words of the query
+//		int totalWords = 0;
+		Iterator<String> words = query.iterator();
+		while(words.hasNext()){
+			String word = words.next();
+			if(queryFrequencies.containsKey(word)){
+				int oldFreq = queryFrequencies.get(word);
+				queryFrequencies.put(word, oldFreq+1);
+			}else{
+				queryFrequencies.put(word, 1);
+			}
+//			totalWords++;
+		}
+		
+		//Declare the "shared" HashMap and norm in which the threads are going to write.
+		Map<String,Double> vector = Collections.synchronizedMap(new HashMap<String,Double>());
+		MutableDouble norm = new MutableDouble();
+		
+		//Compute the query vector and norm
+		computeQueryVector(vector, norm, query, index);
+		
+		queryNorm = Math.sqrt(norm.get());
+		queryVector = vector;
+		System.out.println("vector[query]: "+queryVector);
+		System.out.println("norm[query]: squareRoot(Σ(weight^2)) = "+queryNorm);
 	}
 	
 	public static void main(String[] args){
+				
+		System.out.println("Remember: N  = #documents (without query)\n          nt = #documents that contain word (without query)");
+		System.out.println("Remember: weight = [1+ln(freq)]*ln(1+N/nt)\n\n");
 		
 		String docs[] = 
 		{			
-			"ant ant bee cat dog dog dog elephant fox goat apple room",
-			"apple apple apple apple orange banana banana banana melon peach grape pineapple dog room",
-			"room chair sofa window desk table bed bed dog ant"
+			"ο κομήτης του Χάλλεϋ μας επισκέπτεται περίπου κάθε εβδομήντα έξι χρόνια",
+			"ο κομήτης του Χάλλεϋ ανακαλύφθηκε από τον αστρονόμο Έντμοντ Χάλλεϋ",
+			"ένας κομήτης διαγράφει ελλειπτική τροχιά",
+			"ο πλανήτης Άρης έχει δύο φυσικούς δορυφόρους το Δείμο και το Φόβο",
+			"ο πλανήτης Δίας έχει εξήντα τρείς γνωστούς φυσικούς δορυφόρους",
+			"ο Ήλιος είναι ένας αστέρας",
+			"ο Άρης είναι ένας πλανήτης του ηλιακού μας συστήματος"
 		};
 		
 		QueryProcessor qp = new QueryProcessor(docs);
 		
-		qp.setQuery("ant bee goat chair melon");
+		//Set the query
+		ArrayList<String> query = new ArrayList<String>();
+		query.add("κομήτης");
+		query.add("Χάλλεϋ");
+		qp.setQuery(query);
 		
 		System.out.println("================================================================================================================================\nComputing vector weights and norm for each document. (a vector contains weights for ALL words of the document) ..."); 
 		
 		//For each document
 		for(int i=0;i<docs.length;i++){
-		
-			Vector vector = qp.computeVector(docs[i], i , qp.index);
-			qp.norms[i] = vector.getNorm();
-			qp.vectors.put(i, vector);
-
+			
+			System.out.println("\n\n\n\n\n\n"+" doc"+i+"\n"+docs[i]);
+			
+			//Compute the total number of words this document has.
+			int totalWords =0;
+			ArrayList<String> words = new ArrayList<String>();
+			StringTokenizer tok = new StringTokenizer(docs[i]);
+			while(tok.hasMoreTokens()){
+				words.add(tok.nextToken());
+				totalWords++;
+			}
+			
+			//Declare the "shared" HashMap and norm in which all threads will write (this HashMap is the "vector" that contains word weights inside this document).
+			Map<String,Double> sharedVector = Collections.synchronizedMap(new HashMap<String,Double>());
+			MutableDouble sharedNorm = new MutableDouble();
+			
+			int threads = 1;
+			
+			//Define how many words each thread will take.
+			int wordsPerThread = totalWords/threads;
+			int wordsForFirstThread = wordsPerThread;
+			wordsForFirstThread += (totalWords%threads);
+			int start=0;
+			int end=wordsForFirstThread;
+			
+			//Give a portion to the First thread
+			System.out.println("\n---------------------------------------------------------------\n"+"portion: ["+start+","+end+")");
+			System.out.println("Thread1 takes: "+words.subList(start, end));
+			qp.computeVector(sharedVector, sharedNorm, words.subList(start, end), i , qp.index);
+			
+			//Give a portion to the Rest of the threads
+			for(int t=0;t<threads-1;t++){
+				start = end;
+				end += wordsPerThread;
+				System.out.println("\n---------------------------------------------------------------\n"+"portion: ["+start+","+end+")");
+				System.out.println("Thread"+(t+2)+" takes: "+words.subList(start, end));
+				qp.computeVector(sharedVector, sharedNorm, words.subList(start, end), i , qp.index);
+			}
+			
+			//Join the threads.
+			//TODO : Join the threads
+			
+			//Store final result
+			qp.norms[i] = Math.sqrt(sharedNorm.get());
+			qp.vectors.put(i, new Vector(sharedVector,sharedNorm.get()));
+			System.out.println("vector["+i+"]: "+qp.vectors.get(i).getVector());
+			System.out.println("norm["+i+"]: squareRoot(Σ(weight^2)) = "+qp.norms[i]);
+			
 		}
 		
 		System.out.println("\n\n\n================================================================================================================================\nComputing similarity ...\n");
 		
-		//For each query word, for each doc that contains that word, add something to it's similarity.
+		qp.computeSimilarities();
 		
-		//For each query word
-		for (Entry<String, Double> queryTerm : qp.queryVector.entrySet()){
-					
-					String word = queryTerm.getKey();
-					
-					//For each document that contains this word
-					for (Entry<Integer, MutableInt> doc : qp.index.getHashMap().get(word).entrySet()){
-						
-						int docID = doc.getKey();
-						
-						//Read the weight of this word in the query and in the document
-						double weightOfWordInQuery = qp.queryVector.get(word);
-						double weightOfWordInDocument = qp.vectors.get(docID).getVector().get(word);
-						
-						//Add something to the similarity of this document (with the query)
-						if(qp.similarity.containsKey(docID)){
-							qp.similarity.put(docID, qp.similarity.get(docID) + (weightOfWordInQuery*weightOfWordInDocument));
-						}else{
-							qp.similarity.put(docID,weightOfWordInQuery*weightOfWordInDocument);
-						}
-					}
-		}
+		System.out.println("\n\n\n\n");
 		
 		//Print normalized similarities
 		for (Entry<Integer, Double> similarities : qp.similarity.entrySet()){
-		
-			int docID = similarities.getKey();
-			double notNormalizedSimilarity = similarities.getValue();
 			
-			double docNorm   = qp.vectors.get(docID).getNorm();
-			double queryNorm = qp.queryNorm;
-			
-			double normalizedSimilarity = notNormalizedSimilarity/(docNorm*queryNorm);
-			
-			System.out.println("similarity("+docID+",query): "+normalizedSimilarity);
+			System.out.println("similarity("+similarities.getKey()+",query): "+similarities.getValue());
 		}
-		
+
 	}
-	
+
 }
